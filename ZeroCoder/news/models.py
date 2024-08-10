af6c34f847af28efbb2debf60da809d4dcacc400
MoonWalker404 <crossbeat71@gmail.com>
2024-08-11 01:33:09 +0300

    add homework lessone 3, homework3

---
+++ ZeroCoder/news/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User  # Импорт модели пользователя
+from django.conf import settings
+
+
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор', default=1)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
